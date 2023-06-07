7175dfe66ff2732587010448168a2732ac4749db
Lassa <theopodoro@gmail.com>
2023-06-07 09:04:11 +0200

    Push de musique

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title> index </title>
+        <title> Mélodiz </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
